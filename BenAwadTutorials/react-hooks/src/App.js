1c840ace01235eee04b411b8bd6d48c97bebd46c
PellyRax <67081101+PellyRax@users.noreply.github.com>
2022-07-18 21:22:02 -0500

    use hook

---
+++ BenAwadTutorials/react-hooks/src/App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import { useForm } from './customHooks/useForm';
+
+
+const App = () => {
+  const [values, handleChange] = useForm({ email: '', password:''});
+
+  return (
+    <div>
+      <input name='email' value={values.email} onChange={handleChange}/>
+      <input type='password' name='password' value={values.password} onChange={handleChange}/>
+    </div>
+  );
+}
+
+export default App;
