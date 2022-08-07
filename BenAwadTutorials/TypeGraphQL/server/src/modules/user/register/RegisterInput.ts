260bed2001fd194788cef2dedffc5117ce34b3bf
PellyRax <67081101+PellyRax@users.noreply.github.com>
2022-08-07 18:15:30 -0500

    added logout

--- BenAwadTutorials/TypeGraphQL/server/src/modules/user/register/RegisterInput.ts
+++ BenAwadTutorials/TypeGraphQL/server/src/modules/user/register/RegisterInput.ts
@@ -2,10 +2,10 @@ import { IsEmail, Length } from "class-validator";
 import { Field, InputType } from "type-graphql";
 
 import { IsEmailAlreadyExist } from "./IsEmailAlreadyExist";
-import { PasswordInput } from "../../shared/PasswordInput";
+import { PasswordMixin } from "../../shared/PasswordInput";
 
 @InputType()
-export class RegisterInput extends PasswordInput {
+export class RegisterInput extends PasswordMixin(class {}) {
     @Field()
     @Length(1, 255)
     firstName: string;
